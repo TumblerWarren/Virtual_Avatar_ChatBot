518c7d8d8e630b1894bc333989fdf2fe51a5a4ad
TumblerWarren <137818183+TumblerWarren@users.noreply.github.com>
2023-08-06 12:24:36 +0530

    Add files via upload

---
+++ voicevox_startup.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+
+REM Get the current directory of the batch file
+set "SCRIPT_DIR=%~dp0"
+
+REM Set the log file path
+set "LOG_FILE=%SCRIPT_DIR%\log.txt"
+
+REM Change to the script directory
+cd /d "%SCRIPT_DIR%"
+
+REM Create and activate the main virtual environment
+python -m venv venv
+call venv\Scripts\activate
+
+REM Install PyTorch, torchvision, and torchaudio from a specific index URL
+python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 2>> "%LOG_FILE%"
+
+REM Install openai-whisper from the GitHub repository
+python -m pip install git+https://github.com/openai/whisper.git 2>> "%LOG_FILE%"
+
+REM Install Playwright
+python -m playwright install 2>> "%LOG_FILE%"
+
+REM Install the remaining dependencies from requirements.txt
+python -m pip install -r requirements.txt 2>> "%LOG_FILE%"
+
+REM Start main.py in the background
+start /B python main.py
+
+
+start /B python "%SCRIPT_DIR%\API\voice_vox_api.py"
+
+
+REM Deactivate the virtual environment
+deactivate
+
+REM Display message and prompt user to exit
+echo.
+echo Batch file execution completed. Press any key to exit.
+pause >nul
+
+endlocal
