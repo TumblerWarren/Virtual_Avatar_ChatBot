3b2a78ba478195469269cd5017aff6ef1ac22627
TumblerWarren <thewarrendummy@gmail.com>
2024-12-03 00:33:32 +0530

    Fixed character ai api and added offline TTS

--- utils/Offline_tts.py
+++ utils/Offline_tts.py
@@ -1,19 +1,63 @@
-from TTS.api import TTS
+import torch
+from transformers.models.speecht5.number_normalizer import EnglishNumberNormalizer
+from string import punctuation
+import re
+import soundfile as sf
+from parler_tts import ParlerTTSForConditionalGeneration
+from transformers import AutoTokenizer, AutoFeatureExtractor, set_seed
 import os
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+FILENAME = "local_tts_output.mp3"
+OUTPUT_PATH = os.path.join(current_directory, "resource", "voice_out", FILENAME)
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+# Check if GPU is available and set device
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# Construct the path to the file in the 'utils' folder
-FILE_PATH= os.path.join(script_dir, "", "", "resource", "voice_out", "local_tts_output.wav")
+# Load model, tokenizer, and feature extractor
+repo_id = "parler-tts/parler-tts-mini-expresso"
+model = ParlerTTSForConditionalGeneration.from_pretrained(repo_id).to(device)
+tokenizer = AutoTokenizer.from_pretrained(repo_id)
+feature_extractor = AutoFeatureExtractor.from_pretrained(repo_id)
 
-# List available 🐸TTS models and choose the first one
-'''Useable models:- ['tts_models/en/ek1/tacotron2','tts_models/en/ljspeech/tacotron2-DDC','tts_models/en/ljspeech/tacotron2-DDC_ph',
-'tts_models/en/ljspeech/glow-tts','tts_models/en/jenny/jenny']
-'''
+# Constants
+SAMPLE_RATE = feature_extractor.sampling_rate
+SEED = 42
 
-def voice_generation(responded_text):
+# Preprocessing function
+number_normalizer = EnglishNumberNormalizer()
 
-    tts = TTS(model_name='tts_models/en/ljspeech/tacotron2-DDC_ph', progress_bar=False, gpu=False)
-    tts.tts_to_file(text= responded_text, emotion="happy",file_path=FILE_PATH)
-    return FILE_PATH
+
+def preprocess(text):
+    text = number_normalizer(text).strip()
+    if text[-1] not in punctuation:
+        text = f"{text}."
+
+    abbreviations_pattern = r'\b[A-Z][A-Z\.]+\b'
+
+    def separate_abb(chunk):
+        chunk = chunk.replace(".", "")
+        return " ".join(chunk)
+
+    abbreviations = re.findall(abbreviations_pattern, text)
+    for abv in abbreviations:
+        if abv in text:
+            text = text.replace(abv, separate_abb(abv))
+    return text
+
+
+# TTS generation function
+def voice_generation(text):
+    description = "Elisabeth speaks happily at a slightly slower than average pace with high quality audio."
+    inputs = tokenizer(description, return_tensors="pt").to(device)
+    prompt = tokenizer(preprocess(text), return_tensors="pt").to(device)
+
+    set_seed(SEED)
+    generation = model.generate(input_ids=inputs.input_ids, prompt_input_ids=prompt.input_ids)
+    audio_arr = generation.cpu().numpy().squeeze()
+
+    # Save the generated audio
+    sf.write(OUTPUT_PATH, audio_arr, SAMPLE_RATE)
+    return OUTPUT_PATH
+
+
